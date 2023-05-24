6b1425bd73f43d84a320635860e7d33b97bed0de
Ricardo Cerda <rdocerda@gmail.com>
2023-05-24 17:09:55 -0400

    Initial commit

---
+++ assets/plugins/bootstrap-datepicker/datepicker-init.js
@@ -0,0 +1,15 @@
+$(function () {
+	$('#dp1').datepicker({
+		format: 'dd-mmm-yyyy',
+		language: 'es'
+	});
+	$('#dp2').datepicker({
+		format: 'dd-mmm-yyyy',
+		language: 'es'
+	});
+	$('#dp3').datepicker({
+		format: 'dd-mmm-yyyy',
+		language: 'es'
+	});
+
+});
